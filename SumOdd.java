68e49e78cb6f29c3781f708134a2d3fb66cc15bb
Harshika Devadas <84027574+harshi-1@users.noreply.github.com>
2021-10-06 16:18:38 +0530

    Add files via upload

---
+++ SumOdd.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class SumOdd
+{
+public static void Main(String args[])
+{
+void read()
+File f1=new File("\\home\\exam\\Desktop\\odd.txt");
+fileInput fin=new fileInput(f1);
+fileReader fr=new fileReader(fin);
+bufferReader fb=new bufferReader(fr);
+Scanner sc=new Scanner(System.in);
+system.out.println("enter the number:");
+read sc=sc.nextInt();
+Scanner sd=new Scanner(System.in);
+split(" ");
+int j,k,n;
+for(i=1;i<=l;i++)
+{
+  for(j=1;j<=l;j++)
+  {
+  if(sum % 1)
+  {
+  sum=i+j;
+  }
+Scanner su=new Scanner(System.in);
+system.out.println("sum :" +sum);
+}fin.close();
+}
+}
+}  
+
+
