40003ac2731a3e94435fb852844fe60b7d59834c
Harshika Devadas <84027574+harshi-1@users.noreply.github.com>
2021-10-06 16:18:24 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.scanner;
+class Anum
+{
+int a[n]=n;
+}
+public class Array
+{
+public static void Main(string args[]){throw UserException;
+try{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the limit:");
+sc.nextInt();
+int read;
+Scanner si=new Scanner(system.in);
+System.out.println("enter values:" +a[i]);
+read()=a[i].nextInt();
+Scanner si=new Scanner(system.in);
+System.out.println("least integer:" +a[i]);
+Anum(i);
+if(a[i]<n)
+Anum()=a[i].nextInt();
+Scanner s=new Scanner(system.in);
+System.out.println("highest integer:" +a[i]);
+Anum(i);
+if(a[i]>n)
+Anum()=a[i].nextInt(); 
+Scanner si=new Scanner(system.in);
+System.out.println("average" +a[i]/n);
+Anum(i,j)=a[i].nextInt();
+}
+catch(ArithemeticIOException);
+{
+system.out.println("error in the arithemetic opertion");
+system.out.println("invalid");
+}
+}
+}
+
+
+
+
+
+
+
+
+
